ab03e61e2d6cc39d54d0dadee489d3135441f779
Mason Lane <github@masonlane.me>
2025-06-06 08:23:17 -0500

    Add contact page

---
+++ pages/contact.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+onMounted(() => {
+  document.title = 'Contact'
+})
+</script>
+
+<template>
+  <div class="contact">
+    <title>Contact</title>
+    <h1>Want to contact me?</h1>
+    <a href="mailto:contact@masonscape.dev">
+      <h2>contact@masonscape.dev</h2>
+    </a>
+    <br>
+    <p>Questions, comments, anything is welcome!</p>
+  </div>
+</template>
+
+<style scoped>
+.contact {
+  text-align: center;
+  width: 90vw;
+  text-wrap: wrap;
+  word-wrap: break-word;
+  display: inline-block;
+}
+</style>
