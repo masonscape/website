d12501980f3b98b1e641770de4c8d9a158be22d0
Mason Lane <github@masonlane.me>
2025-06-06 08:22:52 -0500

    Add main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-
+onMounted(() => {
+  document.title = 'masonscape.dev'
+})
 </script>
 
 <template>
-  <h1>test you are at /</h1>
+  <div class="main-page">
+    <h1>Welcome to the masonscape!</h1>
+    <h2>This is a placeholder page. I'll add some cool stuff later.</h2>
+    <h3>For now, check out the <NuxtLink to="/blog">blog!</NuxtLink></h3>
+  </div>
 </template>
 
 <style>
+.main-page {
+  text-align: center;
+}
 </style>
